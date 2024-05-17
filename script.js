98be3d71d5a86f6fab6c3e072ec6f35516342196
notdule <148584497+notdule@users.noreply.github.com>
2024-05-17 22:40:02 +0200

    Switch update

--- script.js
+++ script.js
@@ -198,4 +198,10 @@ const setPlayerCards = (arr = players) => {
 
 playersDropdownList.addEventListener("change", (e) => {
   playerCards.innerHTML = "";
+  
+  switch (e.target.value) {
+    case "nickname":
+      setPlayerCards(players.filter((player) => player.nickname !== null));
+      break;
+    }
 });
